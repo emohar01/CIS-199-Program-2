d71c1828da46c176596b1f0d5c6b0d21fe326ef4
emohar01 <45211621+emohar01@users.noreply.github.com>
2018-11-20 13:59:50 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Project_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.freshmanRadioButton = new System.Windows.Forms.RadioButton();
+            this.sophomoreRadioButton = new System.Windows.Forms.RadioButton();
+            this.juniorRadioButton = new System.Windows.Forms.RadioButton();
+            this.seniorRadioButton = new System.Windows.Forms.RadioButton();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(24, 38);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(61, 13);
+            this.lastNameLabel.TabIndex = 0;
+            this.lastNameLabel.Text = "Last Name:";
+            // 
+            // freshmanRadioButton
+            // 
+            this.freshmanRadioButton.AutoSize = true;
+            this.freshmanRadioButton.Location = new System.Drawing.Point(28, 30);
+            this.freshmanRadioButton.Name = "freshmanRadioButton";
+            this.freshmanRadioButton.Size = new System.Drawing.Size(71, 17);
+            this.freshmanRadioButton.TabIndex = 2;
+            this.freshmanRadioButton.TabStop = true;
+            this.freshmanRadioButton.Text = "Freshman";
+            this.freshmanRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // sophomoreRadioButton
+            // 
+            this.sophomoreRadioButton.AutoSize = true;
+            this.sophomoreRadioButton.Location = new System.Drawing.Point(28, 53);
+            this.sophomoreRadioButton.Name = "sophomoreRadioButton";
+            this.sophomoreRadioButton.Size = new System.Drawing.Size(79, 17);
+            this.sophomoreRadioButton.TabIndex = 3;
+            this.sophomoreRadioButton.TabStop = true;
+            this.sophomoreRadioButton.Text = "Sophomore";
+            this.sophomoreRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // juniorRadioButton
+            // 
+            this.juniorRadioButton.AutoSize = true;
+            this.juniorRadioButton.Location = new System.Drawing.Point(28, 76);
+            this.juniorRadioButton.Name = "juniorRadioButton";
+            this.juniorRadioButton.Size = new System.Drawing.Size(53, 17);
+            this.juniorRadioButton.TabIndex = 4;
+            this.juniorRadioButton.TabStop = true;
+            this.juniorRadioButton.Text = "Junior";
+            this.juniorRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // seniorRadioButton
+            // 
+            this.seniorRadioButton.AutoSize = true;
+            this.seniorRadioButton.Location = new System.Drawing.Point(28, 99);
+            this.seniorRadioButton.Name = "seniorRadioButton";
+            this.seniorRadioButton.Size = new System.Drawing.Size(55, 17);
+            this.seniorRadioButton.TabIndex = 5;
+            this.seniorRadioButton.TabStop = true;
+            this.seniorRadioButton.Text = "Senior";
+            this.seniorRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(100, 35);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lastNameTextBox.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.seniorRadioButton);
+            this.groupBox1.Controls.Add(this.juniorRadioButton);
+            this.groupBox1.Controls.Add(this.freshmanRadioButton);
+            this.groupBox1.Controls.Add(this.sophomoreRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(62, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(138, 132);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Class Standing:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Show Registration";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 337);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.RadioButton freshmanRadioButton;
+        private System.Windows.Forms.RadioButton sophomoreRadioButton;
+        private System.Windows.Forms.RadioButton juniorRadioButton;
+        private System.Windows.Forms.RadioButton seniorRadioButton;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,198 @@
+﻿// A5508
+// Program 2
+// Due: March 9, 2017
+// CIS 199-01
+// Description: This program contains the use of radio buttons to make a registration schedule.  The 4 radio buttons include:
+// freshman, sophomore, junior, and senior.  The "Show Registration" button displays the date and the time that you regitser  
+// classes for depending on what your class standing is and the first letter of your last name.
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            //holds dates of possible registration dates
+            const string date1 = "Wednesday, March 29th"; 
+            const string date2 = "Thursday, March 30th";
+            const string date3 = "Friday, March 31st";
+            const string date4 = "Monday, April 3rd";
+            const string date5 = "Tuesday, April 4th";
+            const string date6 = "Wednesday, April 5th";
+
+            //holds times for the registration dates
+            const string time1 = "8:30 A.M.";
+            const string time2 = "10:00 A.M.";
+            const string time3 = "11:30 A.M.";
+            const string time4 = "2:00 P.M.";
+            const string time5 = "4:00 P.M.";
+
+            string lastName = lastNameTextBox.Text; // lets last name textbox hold last name string
+            char letter; // contains variable for first letter of last name
+            string dateOfReg; // contains registration date
+
+
+            if ((lastNameTextBox.TextLength > 0) && char.IsLetter(lastName[0]))
+            {
+                letter = lastName[0]; // reserved the first letter of the last name for variable 
+                letter = char.ToUpper(letter); // first letter of last name is uppercase
+
+                
+                if (seniorRadioButton.Checked || juniorRadioButton.Checked)// If user selects senior or junior radio button
+                {
+                    if (seniorRadioButton.Checked)
+                    {
+                                            // use first date if senior radio button is clicked
+                        dateOfReg = date1;
+                    }
+                    else
+                    {
+                                            // use second date if junior radio button is clicked
+                        dateOfReg = date2;
+                    }
+
+                                            // If last name is between A-D, show registration date and time
+                    if (letter <= 'D')
+                    {
+                        MessageBox.Show(dateOfReg + " at " + time3);
+                    }
+
+                                            // If last name is between E-I, show registration date and time
+                    else if (letter <= 'I')
+                    {
+                        MessageBox.Show(dateOfReg + " at " + time4);
+                    }
+
+                                            // If last name is between F-O, show registration date and time
+                    else if (letter <= 'O')
+                    {
+                        MessageBox.Show(dateOfReg + " at " + time5);
+                    }
+
+                                            // If last name is between P-S, show registration date and time
+                    else if (letter <= 'S')
+                    {
+                        MessageBox.Show(dateOfReg + " at " + time1);
+                    }
+
+                                            // If last name is between T-Z, show registration date and time
+                    else
+                    {
+                        MessageBox.Show(dateOfReg + " at " + time2);
+                    }
+                }
+
+
+                
+                if (sophomoreRadioButton.Checked || freshmanRadioButton.Checked)// If user selects sophomore or freshman radio button
+                {
+                    if (sophomoreRadioButton.Checked && (letter > 'B') && (letter <= 'O'))
+                    {
+                                            // if sophomore last name letter is greater than B and less than or equal to O,
+                                            // use fourth date
+                        dateOfReg = date4;
+                    }
+                    else if (sophomoreRadioButton.Checked)
+                    {
+                                            // use third date for all other sophomores
+                        dateOfReg = date3;
+                    }
+                    else if (freshmanRadioButton.Checked && (letter > 'B') && (letter <= 'O'))
+                    {
+                                            // if freshman last name letter is greater than B and less than or equal to O,
+                                            // use sixth date
+                        dateOfReg = date6;
+                    }
+                    else
+                    {
+                                            
+                        dateOfReg = date5;// use fifth date for all other freshman
+                    }
+
+                    if (letter <= 'B')
+                    {
+                                            
+                        MessageBox.Show(dateOfReg + " at " + time5);  // display registration date for last names A-B
+                    }
+                    else if (letter <= 'D')
+                    {
+                        
+                        MessageBox.Show(dateOfReg + " at " + time1);  // display registration date for last names C-D
+                    }
+                    else if (letter <= 'F')
+                    {
+                        
+                        MessageBox.Show(dateOfReg + " at " + time2);  // display registration date for last names E-F
+                    }
+                    else if (letter <= 'I')
+                    {
+                        
+                        MessageBox.Show(dateOfReg + " at " + time3);  // display registration date for last names G-I
+                    }
+                    else if (letter <= 'L')
+                    {
+                        
+                        MessageBox.Show(dateOfReg + " at " + time4);  // display registration date for last names J-L
+                    }
+                    else if (letter <= 'O')
+                    {
+                        
+                        MessageBox.Show(dateOfReg + " at " + time5);  // display registration date for last names M-O
+                    }
+                    else if (letter <= 'Q')
+                    {
+                        
+                        MessageBox.Show(dateOfReg + " at " + time1);  // display registration date for last names P-Q
+                    }
+                    else if (letter <= 'S')
+                    {
+                        
+                        MessageBox.Show(dateOfReg + " at " + time2);  // display registration date for last names R-S
+                    }
+                    else if (letter <= 'V')
+                    {
+                        
+                        MessageBox.Show(dateOfReg + " at " + time3);  // display registration date for last names T-V
+                    }
+                    else
+                    {
+                        
+                        MessageBox.Show(dateOfReg + " at " + time4);  // display registration date for last names W-Z
+                    }
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid last name.");  // Enter valid last name 
+            }
+        }
+            }
+
+            
+
+
+            
+        }
+    
+
